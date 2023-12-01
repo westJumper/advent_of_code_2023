179b91bfa2a326e636cb5559aeff454b52b92c90
westJumper <miroslav.skocik@gmail.com>
2023-12-01 14:34:52 +0100

    Day 1 part 1 done

---
+++ part2.js
@@ -0,0 +1,23 @@
+const f = require('fs')
+const readline = require('readline')
+const { isNumberObject } = require('util/types')
+var inputFile = 'input.txt'
+
+var reader = readline.createInterface({
+  input: f.createReadStream(inputFile),
+})
+
+var sum = 0
+
+reader.on('line', function (line) {
+  var eachChar = line.split('')
+  var first = eachChar.find((char) => !isNaN(Number(char)))
+  var last = eachChar.reverse().find((char) => !isNaN(Number(char)))
+
+  var both = first.toString() + last.toString()
+  sum = sum + Number(both)
+})
+
+reader.on('close', function () {
+  console.log(sum)
+})
